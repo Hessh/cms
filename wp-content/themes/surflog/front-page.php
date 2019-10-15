69cc65de9b8252e4a4f4f0b3ad0713efa403a56d
Hessh <haakon.hess@gmail.com>
2019-10-15 14:44:48 +0200

    yes

--- wp-content/themes/surflog/front-page.php
+++ wp-content/themes/surflog/front-page.php
@@ -1,6 +1,13 @@
+<?php get_header(); ?>
 
-<?php 
-get_header(); 
-get_footer();
-?>
+<div class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 col-sm-12" style="background-color: red">hei</div>
+            <div class="col-lg-10 col-sm-12" style="background-color: blue">hade</div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
 
