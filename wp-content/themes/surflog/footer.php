9b0f613e5f2d281d10cf043aa3ee53f0471b78f3
Stine Mathisen <stinemathisen@Stines-MacBook-Pro.local>
2019-10-15 15:37:59 +0200

    mer blæ

--- wp-content/themes/surflog/footer.php
+++ wp-content/themes/surflog/footer.php
@@ -16,7 +16,9 @@
 	<footer class="row">
 		<div class="col-4">
 			om oss
-			Surf Guru markedsfører for ulike aktører som selger surfeturer. Våre aktører er kjent for å tilby de mest opplevelsesrike turene,
+			Surf Guru markedsfører for ulike aktører som selger surfeturer. 
+			Våre aktører er kjent for å tilby de beste og mest opplevelsesrike turene.
+			
 		</div>
 		<div class="col-4">
 			personvern
