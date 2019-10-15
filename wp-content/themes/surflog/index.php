637e86f41e53a285cfa24d32fdda0ee8a13d6160
Hessh <haakon.hess@gmail.com>
2019-10-15 13:54:01 +0200

    backtracking

--- wp-content/themes/surflog/index.php
+++ wp-content/themes/surflog/index.php
@@ -13,55 +13,6 @@
  */
 
 get_header();
-$count_posts = wp_count_posts();
-
-$published_posts = $count_posts->publish;
-
-$number_of_pages = ceil($published_posts / $posts_per_page);
-
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-?>
-<div id="blogg-container">
-    <div class="blogg-table">
-        <div class="row title-row">
-            <div class="col-12">
-                <h1 class="main-title text-center">BLOGG</h1>
-            </div>
-        </div>
-        <div class="blogg-row">
-            <?php
-            if (have_posts()) :
-                while (have_posts()) : the_post();
-                    ?>
-            <div class="article-box">
-                <div class="blogg-banner">
-                    <a href="<?php the_permalink() ?>">
-                        <img src="<?php if (has_post_thumbnail()) {
-                                                the_post_thumbnail_url();
-                                            } ?>"></a>
-                </div>
-                <a href="<?php the_permalink() ?>">
-                    <?php the_title('<h3>', '</h3>', true); ?>
-                    <?php the_excerpt() ?>
-                    <button>Les mer</button>
-                </a>
-            </div>
-            <?php
-                endwhile;
-            endif;
-            ?>
-        </div>
-        <div id="blogg-nav">
-            <?php previous_posts_link("<");?>
-            <span id="page-counter"><?php echo $paged; ?> / <?php echo $number_of_pages ?></span>
-            <?php next_posts_link(">");?>
-        </div>
-    </div>
-</div>
-<?php get_footer() ?>
-
-<?php/*
-get_header();
 ?>
 
 	<div id="primary" class="content-area">
@@ -79,15 +30,15 @@ get_header();
 			endif;
 
 			/* Start the Loop */
-			/*while ( have_posts() ) :
+			while ( have_posts() ) :
 				the_post();
 
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 *
-				*get_template_part( 'template-parts/content', get_post_type() );
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
 
@@ -105,5 +56,4 @@ get_header();
 
 <?php
 get_sidebar();
-get_footer();*/
-?>
+get_footer();
