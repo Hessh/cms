b8423adfecb1560face1a6caec06f5878d569b77
Hessh <haakon.hess@gmail.com>
2019-10-15 15:15:40 +0200

    frame

--- wp-content/themes/surflog/footer.php
+++ wp-content/themes/surflog/footer.php
@@ -13,20 +13,16 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'surflog' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'surflog' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'surflog' ), 'surflog', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+	<footer class="row">
+		<div class="col-4">
+			om oss
+		</div>
+		<div class="col-4">
+			personvern
+		</div>
+		<div class="col-4">
+			kontakt
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
