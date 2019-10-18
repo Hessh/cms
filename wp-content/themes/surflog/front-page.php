9b1f922982f7701afae8498f7ebc3e5ec175a585
Hessh <haakon.hess@gmail.com>
2019-10-18 17:18:11 +0200

    changed classnames from blogg to product

--- wp-content/themes/surflog/front-page.php
+++ wp-content/themes/surflog/front-page.php
@@ -1,12 +1,12 @@
 <?php get_header("front-page"); ?>
-<div id="blogg-container">
-    <div class="blogg-table">
+<div id="product-container">
+    <div class="product-table">
         <div class="row title-row">
             <div class="col-12">
                 <h1 class="main-title text-center">PRODUCT</h1>
             </div>
         </div>
-        <div class="blogg-row">
+        <div class="product-row">
             <?php
             query_posts('cat=182');
 
@@ -14,7 +14,7 @@
                 while (have_posts()) : the_post();
                     /*if (in_category('182')) {*/
                     ?>
-                    <div class="article-box">
+                    <div class="product-box">
                         <div class="product-banner" style="background-image: url(<?php if (has_post_thumbnail()) {the_post_thumbnail_url();} ?>)">
                             <a class="product-link" href="<?php the_permalink() ?>">
                                 <?php the_title('<h3>', '</h3>', true); ?>
