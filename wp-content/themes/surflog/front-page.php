f907438dc1d15eb9f9549e9732c7f7ebce6a94a0
Hessh <haakon.hess@gmail.com>
2019-10-18 18:37:42 +0200

    Testing

--- wp-content/themes/surflog/front-page.php
+++ wp-content/themes/surflog/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header("front-page"); ?>
-<div id="product-container">
-    <div class="product-table">
+<div id="product-container container-fluid">
+    <div class="product-table container">
         <div class="row title-row">
             <div class="col-12">
                 <h1 class="main-title text-center">PRODUCT</h1>
@@ -14,16 +14,11 @@
                 while (have_posts()) : the_post();
                     /*if (in_category('182')) {*/
                     ?>
-                    <div class="product-box">
-                        <div class="product-banner" style="background-image: url(<?php if (has_post_thumbnail()) {
-                                                                                                the_post_thumbnail_url();
-                                                                                            } ?>)">
-                            <a class="product-link" href="<?php the_permalink() ?>">
-                                <?php the_title('<h3>', '</h3>', true); ?>
-                            </a>
-                        </div>
-                        <a href="<?php the_permalink() ?>">
-
+                    <div class="product-box" style="background-image: url(<?php if (has_post_thumbnail()) {
+                                                                                        the_post_thumbnail_url();
+                                                                                    } ?>)">
+                        <a class="product-link" href="<?php the_permalink() ?>">
+                            <?php the_title('<h3>', '</h3>', true); ?>
                         </a>
                     </div>
             <?php
@@ -40,7 +35,7 @@
     <div class="row">
         <div class="col-push-12 empty"></div>
     </div>
-    <div class="container gallary-table">
+    <div class="gallary-table">
         <div class="title-row">
             <div class="col-12">
                 <h2>BLOG</h2>
