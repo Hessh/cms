31468c15309017cee4d60e6793297d9644ce388e
Sofie Jønsson <sofiegjonsson@gmail.com>
2019-10-19 15:36:35 +0200

    la på ramme på artikkel

--- wp-content/themes/surflog/article-page.php
+++ wp-content/themes/surflog/article-page.php
@@ -7,15 +7,19 @@
 
 <?php get_header($name = null) ?>
 
-<div class="container">
+<div class="container-fluid-gray">
+    <div class="article-container basic">
 
-    <h1><?php the_title();?></h1>
+        <h1><?php the_title();?></h1>
 
 
-    <?php if (have_posts()) : while(have_posts()) : the_post();?>
+        <?php if (have_posts()) : while(have_posts()) : the_post();?>
 
         <?php the_content();?>
 
     <?php endwhile; endif;?>
 
+    </div>
 </div>
+
+<?php get_footer($name = null) ?>
