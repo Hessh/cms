16a9c6278b917597205f45db08e87b8502fe6fe0
Sofie Jønsson <sofiegjonsson@gmail.com>
2019-10-20 01:19:35 +0200

    lage produktpokser

--- wp-content/themes/surflog/product-page.php
+++ wp-content/themes/surflog/product-page.php
@@ -7,15 +7,47 @@
 
 <?php get_header($name = null) ?>
 
-<div class="container">
+<div class="hero">
 
-    <h1><?php the_title();?></h1>
+</div>
+
+<div class="container-fluid-gray">
+
+    <div class="product-container">
+        <div class="product-table">
+            <div class="row title-row">
+                <div class="col-12">
+                    <h1 class="main-title text-center"></h1>
+                </div>
+            </div>
+            <div class="product-row">
+                <div class="product-box">
+                            <div class="product-banner">
+                                <a href="<?php the_permalink() ?>">
+                                    <img src="<?php if (has_post_thumbnail()) {
+                                                                the_post_thumbnail_url();
+                                                            } ?>"></a>
+                            </div>
+                                <a href="<?php the_permalink() ?>">
+                                    <?php the_title('<h3>', '</h3>', true); ?>
+                                    <?php the_excerpt() ?>
+                                    <button>BESTILL</button>
+                                </a>
+                </div>
+            </div>
+        
+
+        <h1><?php the_title();?></h1>
 
 
-    <?php if (have_posts()) : while(have_posts()) : the_post();?>
+             <?php if (have_posts()) : while(have_posts()) : the_post();?>
 
         <?php the_content();?>
 
-    <?php endwhile; endif;?>
+         <?php endwhile; endif;?>
 
+         </div>
+    </div>
 </div>
+
+<?php get_footer($name = null) ?>
