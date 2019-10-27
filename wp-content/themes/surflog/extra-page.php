e85932da014223215d547a517d3be779ac8fdd63
Hessh <haakon.hess@gmail.com>
2019-10-27 20:46:52 +0100

    test 1

--- wp-content/themes/surflog/extra-page.php
+++ wp-content/themes/surflog/extra-page.php
@@ -7,6 +7,9 @@
 
 <?php get_header($name = null) ?>
 <div class="container-fluid">
+    <div class="hero">
+        
+    </div>
     <div class="container">
         <h1><?php the_title(); ?></h1>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
