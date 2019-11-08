b848b3035be9f369ffb254308e904509502c43c0
Hessh <haakon.hess@gmail.com>
2019-11-08 17:52:11 +0100

    Fixed products

--- wp-content/themes/surflog/front-page.php
+++ wp-content/themes/surflog/front-page.php
@@ -18,9 +18,7 @@
                     while (have_posts()) : the_post();
                         /*if (in_category('182')) {*/
                         ?>
-                        <div class="product-box" style="background-image: url(<?php if (has_post_thumbnail()) {
-                                                                                            the_post_thumbnail_url();
-                                                                                        } ?>)">
+                        <div class="product-box" style="background-image: url(<?php if (has_post_thumbnail()) {the_post_thumbnail_url();} ?>)">
                             <a class="product-link" href="<?php the_permalink() ?>">
                                 <?php the_title('<h3>', '</h3>', true); ?>
                             </a>
--- wp-content/themes/surflog/product-page.php
+++ wp-content/themes/surflog/product-page.php
@@ -17,61 +17,65 @@ $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 ?>
 
 <div class="container-fluid">
-    <div class="hero"></div>
+    <div class="hero">
+        <div class="row title-row">
+            <div class="col-12">
+                <h1 class="title text-center"><?php the_title(); ?></h1>
+            </div>
+        </div>
+    </div>
     <div class="container product-container">
-        <div class="product-table">
-            <div class="row title-row">
-                <div class="col-12">
-                    <h1 class="main-title text-center"><?php the_title(); ?></h1>
-                </div>
+        <div class="row info-box product-content">
+            <div class="col-lg-8 col-sm-12 order-sm-one">
+                <div class="destination-image" style="background-image: url('<?php echo get_field('image') ?>')"></div>
             </div>
-            <div class="row info-box">
-                <div class="col-lg-8 col-sm-12 order-sm-one">
-                    <img class="image-destination" src="<?php echo get_field('image') ?>">
+            <div class="col-lg-4 col-sm-12 order-sm-two">
+                <div class="text-container">
+                    <h2>Location</h2>
+                    <?php echo get_field('description_text') ?>
                 </div>
-                <div class="col-lg-4 col-sm-12 order-sm-two">
-                    <div class="text-container">
-                    <h2>Test</h2>
-                    <p><?php echo get_field('description_text') ?></p>
-                    </div>
-                    <div class="maps-container">
-                    <iframe src="<?php echo get_field('google_maps') ?>" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-                    </div>
-                    <div class="button-container">
+                <div class="maps-container">
+                    <iframe class="text-center" src="<?php echo get_field('google_maps') ?>" width="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+                <div class="button-container">
                     <a href="<?php echo get_field('booking_link') ?>" target="_blank"><button>Book your trip now</button></a>
-                    </div>
                 </div>
             </div>
-            <div class="row text-box">
-                <div class="col-12">
-                    <h2 class="text-center">A typical day</h2>
-                    <p><?php echo get_field('typical_day') ?></p>
-                </div>
+        </div>
+        <div class="row text-box product-content">
+            <div class="col-12">
+                <h2 class="text-center">Info</h2>
+                <?php echo get_field('typical_day') ?>
             </div>
-            <div class="row include-box">
-                <div class="col-12">
-                    <h2 class="text-center">Includes</h2>
-                </div>
-                <div class="col-lg-6 col-sm-12">
-                    <h3>Facilities</h3>
-                    <li class="checkmark"><?php echo get_field('facility_1') ?></li>
-                    <li class="checkmark"><?php echo get_field('facility_2') ?></li>
-                    <li class="checkmark"><?php echo get_field('facility_3') ?></li>
-                    <li class="checkmark"><?php echo get_field('facility_4') ?></li>
-                </div>
-                <div class="col-lg-6 col-sm-12">
+            <div class="col-lg-4 col-sm-12">
+                <h3>Facilities</h3>
+                <li class="checkmark"><?php echo get_field('facility_1') ?></li>
+                <li class="checkmark"><?php echo get_field('facility_2') ?></li>
+                <li class="checkmark"><?php echo get_field('facility_3') ?></li>
+                <li class="checkmark"><?php echo get_field('facility_4') ?></li>
+            </div>
+            <div class="col-lg-4 col-sm-12">
                 <h3>Close by</h3>
-                    <li class="checkmark"><?php echo get_field('close_by_1') ?></li>
-                    <li class="checkmark"><?php echo get_field('close_by_2') ?></li>
-                    <li class="checkmark"><?php echo get_field('close_by_3') ?></li>
-                    <li class="checkmark"><?php echo get_field('close_by_4') ?></li>
-                </div>
+                <li class="checkmark"><?php echo get_field('close_by_1') ?></li>
+                <li class="checkmark"><?php echo get_field('close_by_2') ?></li>
+                <li class="checkmark"><?php echo get_field('close_by_3') ?></li>
+                <li class="checkmark"><?php echo get_field('close_by_4') ?></li>
+            </div>
+            <div class="col-lg-4 col-sm-12">
+                <h3>Things to do</h3>
+                <li class="checkmark"><?php echo get_field('todo_1') ?></li>
+                <li class="checkmark"><?php echo get_field('todo_2') ?></li>
+                <li class="checkmark"><?php echo get_field('todo_3') ?></li>
+                <li class="checkmark"><?php echo get_field('todo_4') ?></li>
+            </div>
+            <div class="col-12 button-container">
+                <a href="<?php echo get_field('booking_link') ?>" target="_blank"><button>Book your trip now</button></a>
             </div>
         </div>
     </div>
 </div>
-<?php 
-    include 'assets/includes/surfer.php';
-    include 'assets/includes/post-carousel.php';
-    get_footer();
+<?php
+include 'assets/includes/surfer.php';
+include 'assets/includes/post-carousel.php';
+get_footer();
 ?>
