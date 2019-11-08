794d6e7d604e2a79e9f9ee83678d515ea66e70be
Sofie Jønsson <sofiegjonsson@gmail.com>
2019-11-08 18:13:18 +0100

    changes

--- wp-content/plugins/form/form.php
+++ wp-content/plugins/form/form.php
@@ -11,64 +11,31 @@ License:     GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 defined( 'ABSPATH' ) or die( 'Hey, you dont have access to this file!' );
 
-/*
-defined( 'ABSPATH' ) or die( 'Hey, you dont have access to this file!' );
-
-require_once( __DIR__ . '/includes') '/includes/class-PHPFormBuilder.php' );
-// Main plugin class
-if ( ! class_exists('JMOForms ') ) {
-    class JMOForms {
-        public __construct() {
-            
-        }
-        public function enqueue_scripts() {
-
-        }
-        public function form( $atts ) {
-            global $post;
-
-            $atts = shortcode_atts(
-                array(
-                    'add_honeypot' => false,
-                ), $atts, 'jmoform');
-
-            // Instantiate the form class
-            $form = new PHPFormBBuilder();
-
-            // Set form options
-            $form->set_att()
-        }
-        public function form_handler() {
-
-        }
-    }
-}
-
-$jmoforms = new JMOForms;
-*/
-
 
 function html_form_code() {
     ?>
-	<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
-	<p>Your Name (required)</p><br/>
-	<input type="text" name="name" value="' . ( isset( $_POST["name"] ) ? esc_attr( $_POST["name"] ) : '' ) . '" size="40">
-    
-	<p>Your Email (required)</p><br/>
-    <input type="email" name="email" value="' . ( isset( $_POST["email"] ) ? esc_attr( $_POST["email"] ) : '' ) . '" size="40">
-    
-	<p>	Subject (required)</p><br/>
-	<input type="text" name="subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["subject"] ) ? esc_attr( $_POST["subject"] ) : '' ) . '" size="40" />;
-	</p>;
+	<label for="name">Your Name (required)</label><br/>
+	<input type="text" name="name"><br/>
+	
+	<label for="email">Your Email (required)</label><br/>
+    <input type="email" name="email"><br/>
+	
+	<label for="subject">Subject (required)</label><br/>
+	<input type="text" name="subject" required="required"><br/>
 
-	<p>Your Message (required)</p><br/>
-    <textarea rows="10" cols="35" name="message">' . ( isset( $_POST["message"] ) ? esc_attr( $_POST["message"] ) : '' ) . '</textarea>';
+	<label for="message">Your Message (required)</label><br/>
+    <textarea rows="10" cols="35" name="message"></textarea>
     
 	<button type="submit" name="submitted" value="Send">Send</button>
-    </form>;
-    <?php
+	</form>
+
+	<?php
+
 }
+
+   
 
 function deliver_mail() {
 
@@ -105,6 +72,6 @@ function shortcode() {
 	return ob_get_clean();
 }
 
-add_shortcode( 'sitepoint_contact_form', 'shortcode' );
+add_shortcode( 'sitepoint_form', 'shortcode' );
 
 ?>
