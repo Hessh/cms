6491887ff861efbcb3a4568ef8cb206357cee7f0
Hessh <haakon.hess@gmail.com>
2019-11-09 18:41:50 +0100

    footer

--- wp-content/themes/surflog/footer.php
+++ wp-content/themes/surflog/footer.php
@@ -17,28 +17,17 @@
     <div id="footer-container">
         <div class="row">
             <div class="col-lg-4 col-sm-12">
-                <p class="footer-text">Surf guru er en nettside opprettholdt av et digitalt markedsføringsfirma som skaffer avtaler med aktører innenfor surfeturer. Her vil du ha muligheten til å utforske de ulike surfeturene på en og samme nettside. </p>
-            </div> 
-            <div class="col-push-1 sm-none"></div>
-           <!--<div class="col-lg-3 col-sm-12 footer-content">
-                <h3 class="bold footer-heading">Populære artikler</h3> 
-                <?php/* wp_nav_menu(array(
-					'theme_location' => 'footer-articles',
-					'menu_id' => 'popular-articles',
-					'container' => 'ul',
-                    'menu_class' => 'popular-articles'
-                )); */?> 
-            </div> -->
-            <div class="col-push-1 sm-none"></div>
-            <div class="col-lg-3 col-sm-12 footer-content">
+                <p class="footer-text">SurfGuru is a website maintained by a digital marketing company that obtains deals with variouse surfingcamps around the world. Here you will have the opportunity to explore the various surf trips all in one place</p>
+            </div>
+            <div class="col-push-4 sm-none"></div>
+            <div class="col-lg-4 col-sm-12 footer-content">
                 <h3 class="bold footer-heading">Kontakt</h3>
                 <a href="kontakt" id="email" class="icon footer-text"><span class="material-icons">email</span>kontakt@surfguru.no</a>
-                <p id="location" class="icon footer-text"><span class="material-icons">location_on</span>Bogstadveien 14b,<br />0355 Oslo, Norge</p>
                 <a id="facebook" target="_blank" href="https://www.facebook.com">Nå oss på Facebook</a>
             </div>
         </div>
         <div class="row">
-            <div class="col-12">
+            <div class="col-lg-6 col-sm-12">
                 <?php wp_nav_menu(array(
                     'theme_location' => 'footer-menu',
 					'menu_id' => 'footer-menu',
@@ -46,8 +35,8 @@
                     'menu_class' => 'footer-menu'
                 )); ?>
             </div>
-            <div class="col-12">
-                <p id="footer-info">Surf Guru drives av marketing AS, organisationsnummer  123456789, adress Bogstadveien 14b, 0355 Oslo, Norge.</p>
+            <div class="col-lg-6 col-sm-12">
+                <p id="footer-info">SurfGuru is maintained by Digital Marketing AS</p>
             </div>
         </div>
     </div>
