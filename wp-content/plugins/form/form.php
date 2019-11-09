d41663433abb2126f5234c655fe49046dc47f712
Sofie Jønsson <sofiegjonsson@gmail.com>
2019-11-09 11:45:35 +0100

    drit

--- wp-content/plugins/form/form.php
+++ wp-content/plugins/form/form.php
@@ -11,7 +11,7 @@ License:     GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
 defined( 'ABSPATH' ) or die( 'Hey, you dont have access to this file!' );
 
-
+/*
 function html_form_code() {
     ?>
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
@@ -35,8 +35,6 @@ function html_form_code() {
 
 }
 
-   
-
 function deliver_mail() {
 
 	// if the submit button is clicked, send the email
@@ -49,7 +47,7 @@ function deliver_mail() {
 		$message = esc_textarea( $_POST["message"] );
 
 		// get the blog administrator's email address
-		$to = get_option( 'admin_email' );
+		$to = ( 'sofiegjonsson@gmail.com' );
 
 		$headers = "From: $name <$email>" . "\r\n";
 
@@ -73,5 +71,66 @@ function shortcode() {
 }
 
 add_shortcode( 'sitepoint_form', 'shortcode' );
+
+?> */
+
+function html_form_code() {
+	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="POST">';
+	echo '<p>';
+	echo 'Your Name (required) <br/>';
+	echo '<input type="text" name="name" value="' . ( isset( $_POST["name"] ) ? esc_attr( $_POST["name"] ) : '' ) . '" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Email (required) <br/>';
+	echo '<input type="email" name="email" value="' . ( isset( $_POST["email"] ) ? esc_attr( $_POST["email"] ) : '' ) . '" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Subject (required) <br/>';
+	echo '<input type="text" name="subject" value="' . ( isset( $_POST["subject"] ) ? esc_attr( $_POST["subject"] ) : '' ) . '" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Message (required) <br/>';
+	echo '<textarea rows="10" cols="35" name="message">' . ( isset( $_POST["message"] ) ? esc_attr( $_POST["message"] ) : '' ) . '</textarea>';
+	echo '</p>';
+	echo '<p><input type="submit" name="submitted" value="Send"></p>';
+	echo '</form>';
+}
+
+function deliver_mail() {
+
+	// if the submit button is clicked, send the email
+	if ( isset( $_POST['submitted'] ) ) {
+
+		// sanitize form values
+		$name    =  $_POST["name"];
+		$email   =  $_POST["email"];
+		$subject =  $_POST["subject"];
+		$message =  $_POST["message"];
+
+		// get the blog administrator's email address
+		$to = get_option( 'sofiegjonsson@gmail.com' );
+
+		$headers = "From: $name <$email>" . "\r\n";
+
+		// If email has been process for sending, display a success message
+		if ( wp_mail( $to, $subject, $message, $headers ) ) {
+			echo '<div>';
+			echo '<p>Thanks for contacting me, expect a response soon.</p>';
+			echo '</div>';
+		} else {
+			echo 'An unexpected error occurred';
+		}
+	}
+}
+
+function shortcode() {
+	ob_start();
+	deliver_mail();
+	html_form_code();
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'sitepoint_contact_form', 'shortcode' );
 
 ?>
