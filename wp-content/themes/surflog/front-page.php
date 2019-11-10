6e65331f6338b4fe3a155bd9e010d86c4064dd53
Sofie Jønsson <sofiegjonsson@gmail.com>
2019-11-10 01:11:24 +0100

    prøve og faile

--- wp-content/themes/surflog/front-page.php
+++ wp-content/themes/surflog/front-page.php
@@ -8,33 +8,34 @@
     </div>
     <div class="product-container">
         <div class="product-table">
-
-        <?php $post_objects = get_field('post_objects');
-            if ( $post_objects ): ?>
-
-            <ul> <?php foreach( $post_objects as $post):?>
-        <?php setup_postdata($post); ?> 
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <?php the_field('front page') ?>
-        </li>
-        <?php endforeach; ?>
-    
-    </ul>
-    <?php wp_reset_postdata();?>
-    <?php endif
    
-    ?>
+    <?php 
+            foreach( get_field('image_01') as $post ):
+            setup_postdata( $post );
+            ?>
+
+            <div class="row product-box">
+            <div class="col-lg-8 col-sm-12 order-sm-one">
+                <a href="<?php the_parmalink(); ?>">
+                <?php the_post_thumbnail(); ?>
+                </a>
+
+                <a href="<?php the_parmalink(); ?>">
+                <h3 class="article-title"><?php the_title(); ?></h3>
+                </a>
+               
+            </div>
+        </div>
+        <?php 
+            wp_reset_postdata();
+            endforeach;
+            ?>
+        </div>
+    </div>
 
     <?php
     include 'assets/includes/post-carousel.php';
     ?>
-    </div>
-    </div>
 </div>
-<?php get_footer(); ?>
 
-<!--
-<div class="col-lg-12 col-sm-12 order-sm-one">
-                     <div class="product-image">/div>
-                 </div>
+<?php get_footer(); ?>
